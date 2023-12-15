bc1293a7e0a087bf0255d35c82159da95599c86f
yang <zy892065502@gmail.com>
2023-12-15 22:13:12 +0800

    bookmarks add test

--- js/background.js
+++ js/background.js
@@ -3,4 +3,9 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("Ask Your Bookmarks extension installed.");
 });
 
-// 其他后台任务
+chrome.browserAction.onClicked.addListener(() => {
+    chrome.bookmarks.getTree(bookmarkItems => {
+      console.log(bookmarkItems); // 这里可以处理书签数据
+      // 您可以在这里编写代码来进一步处理或导出书签
+    });
+  });
