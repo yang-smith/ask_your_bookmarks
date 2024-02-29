ea0ccbebb0ed4d904e3fec386f52e91527f8340d
yang <zy892065502@gmail.com>
2024-02-29 11:44:16 +0800

    add navigation bar

--- content-script/App.tsx
+++ content-script/App.tsx
@@ -1,5 +1,5 @@
 import browser from "webextension-polyfill";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import SignIn from "./SignIn";
 import SearchComponent from "./Search";
 import React from "react";
@@ -20,12 +20,12 @@ const App = () => {
   const [error, setError] = useState('');
 
   async function getSession() {
-    const {data: {session}} = await browser.runtime.sendMessage({action: 'getSession'});
-    console.log("getsesion",session);
+    const { data: { session } } = await browser.runtime.sendMessage({ action: 'getSession' });
+    console.log("getsesion", session);
     setSession(session);
   }
   async function getUploadcheck() {
-    const response = await browser.runtime.sendMessage({action: 'getUploadcheck'});
+    const response = await browser.runtime.sendMessage({ action: 'getUploadcheck' });
     setUploadcheck(response.Uploadcheck);
   }
 
@@ -36,24 +36,24 @@ const App = () => {
 
   async function handleOnClick() {
     setLoading(true);
-    const {data} = await browser.runtime.sendMessage({action: 'fetch'});
+    const { data } = await browser.runtime.sendMessage({ action: 'fetch' });
     setFact(data);
     setLoading(false);
   }
 
   async function handleSignUp(email: string, password: string) {
-    await browser.runtime.sendMessage({action: 'signup', value: {email, password}});
+    await browser.runtime.sendMessage({ action: 'signup', value: { email, password } });
     setScreen(SCREEN.SIGN_IN)
   }
 
   async function handleSignIn(email: string, password: string) {
-    const {data, error} = await browser.runtime.sendMessage({action: 'signin', value: {email, password}});
+    const { data, error } = await browser.runtime.sendMessage({ action: 'signin', value: { email, password } });
     if (error) return setError(error.message)
     // console.log(data.session);
     setSession(data.session)
     await getUploadcheck();
     console.log("if uploadcheck: ", uploadcheck);
-    if(uploadcheck){
+    if (uploadcheck) {
       setScreen(SCREEN.SEARCH);
     } else {
       setScreen(SCREEN.UPLOAD);
@@ -61,7 +61,7 @@ const App = () => {
   }
 
   async function handleSignOut() {
-    const signOutResult = await browser.runtime.sendMessage({action: 'signout'});
+    const signOutResult = await browser.runtime.sendMessage({ action: 'signout' });
     setScreen(SCREEN.SIGN_IN);
     setSession(signOutResult.data);
   }
@@ -72,28 +72,28 @@ const App = () => {
         return <SignIn onSignIn={handleSignUp} title={'Sign Up'} onScreenChange={() => {
           setScreen(SCREEN.SIGN_IN);
           setError('');
-        }} helpText={'Got an account? Sign in'} error={error}/>;
-      } 
+        }} helpText={'Got an account? Sign in'} error={error} />;
+      }
       return <SignIn title='Sign In' onSignIn={handleSignIn} onScreenChange={() => {
         setScreen(SCREEN.SIGN_UP)
         setError('');
-      }} helpText={'Create an account'} error={error}/>
-      
+      }} helpText={'Create an account'} error={error} />
+
     }
-    if(screen == SCREEN.UPLOAD) {
-      return <Upload onScreenChange={()=>{
+    if (screen == SCREEN.UPLOAD) {
+      return <Upload onScreenChange={() => {
         setScreen(SCREEN.SEARCH)
       }}></Upload>
-    } else if(screen == SCREEN.SEARCH) {
+    } else if (screen == SCREEN.SEARCH) {
       return <SearchComponent BackToSign={() => {
         setScreen(SCREEN.SIGN_IN)
       }}></SearchComponent>
-    } else if(screen == SCREEN.AI) {
+    } else if (screen == SCREEN.AI) {
       return <AIComponent ChangeToSearch={() => {
         setScreen(SCREEN.SEARCH)
       }
       }></AIComponent>
-    } else if(screen == SCREEN.BOOKMARKS) {
+    } else if (screen == SCREEN.BOOKMARKS) {
       return <BookMarks ChangeToSearch={() => {
         setScreen(SCREEN.SEARCH)
       }}></BookMarks>
@@ -114,10 +114,23 @@ const App = () => {
 
   return (
     // <div className='absolute top-0 left-0'>
-      <div className='flex flex-col gap-4 p-4 shadow-sm bg-gradient-to-r from-purple-100 to-blue-200 w-96 rounded-md'>
-        {/* <h1>Cat Facts!</h1> */}
-        {renderApp()}
+    <div className='flex flex-col gap-4 p-4 shadow-sm bg-gradient-to-r from-purple-100 to-blue-200 w-96 rounded-md'>
+      <div>
+        <div className="font-bold">App Logo</div>
+          <button className="py-2 px-4 bg-blue-400 hover:bg-blue-600 text-white font-semibold rounded-lg shadow transition duration-300" onClick={() => setScreen(SCREEN.SEARCH)}>搜索</button>
+          <button className="py-2 px-4 bg-blue-400 hover:bg-blue-600 text-white font-semibold rounded-lg shadow transition duration-300" onClick={() => setScreen(SCREEN.UPLOAD)}>上传</button>
+          <button className="py-2 px-4 bg-blue-400 hover:bg-blue-600 text-white font-semibold rounded-lg shadow transition duration-300" onClick={() => setScreen(SCREEN.AI)}>AI 功能</button>
+          <button className="py-2 px-4 bg-blue-400 hover:bg-blue-600 text-white font-semibold rounded-lg shadow transition duration-300" onClick={() => setScreen(SCREEN.BOOKMARKS)}>书签</button>
+        {session && (
+          <button className="ml-auto py-2 px-4 border border-white rounded-md hover:bg-white hover:text-blue-700 transition duration-300" onClick={handleSignOut}>
+            登出
+          </button>
+        )}
       </div>
+
+
+      {renderApp()}
+    </div>
     // </div>
   )
 }
