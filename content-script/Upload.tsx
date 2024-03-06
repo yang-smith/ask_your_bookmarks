1fb16359f7c92039ce67daaeac90344e624b6f9d
yang <zy892065502@gmail.com>
2024-03-06 11:41:50 +0800

    improve upload UI

--- content-script/Upload.tsx
+++ content-script/Upload.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Formik, Form, Field } from "formik";
 import browser from "webextension-polyfill";
 
-const Upload = ({onScreenChange}) => {
-  const [loading, setLoading] = useState(true);
-  const [progress, setProgress] = useState(0); 
+const Upload = ({ onScreenChange }) => {
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-  const handleStartUpload = () =>{
+  const handleStartUpload = () => {
     browser.runtime.sendMessage({ action: 'startUpload' });
+    setLoading(true);
   }
 
   useEffect(() => {
@@ -29,22 +30,24 @@ const Upload = ({onScreenChange}) => {
     };
 
     // 定时请求进度信息
-    const intervalId = setInterval(requestProgress, 1000); 
+    const intervalId = setInterval(requestProgress, 1000);
 
     // 清理函数，组件卸载时清除定时器
     return () => {
       clearInterval(intervalId);
     };
   }, []);
-  
+
   return (
     <>
-      <h1 className="text-lg font-bold text-slate-800 py-6">Restruct your bookmarks</h1>
-      <div className='flex flex-col gap-y-2'>
-        <button onClick={handleStartUpload}>
-            start
+      <div className='flex flex-col items-center gap-y-4 py-6'>
+        <button
+          className="bg-indigo-500 text-white text-lg font-bold py-2 px-4 rounded hover:bg-indigo-700 focus:outline-none focus:shadow-outline"
+          onClick={handleStartUpload}>
+          Reconstruct Your Old Bookmarks
         </button>
-        {loading && (
+
+        {progress>1 && (
           <>
             <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
               <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${progress}%` }}> Doing... {progress}% </div>
@@ -52,11 +55,8 @@ const Upload = ({onScreenChange}) => {
             <p className="text-slate-700">Please wait, in progress...</p>
           </>
         )}
-        {!loading && (
+        {progress>=100 && (
           <p className="text-slate-700">Completed!</p>
-        )}
-        { (
-          <p className={'font-bold text-orange-600'}></p>
         )}
       </div>
     </>
