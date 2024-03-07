dcb43ab227f3b2c54dc321f1b3e23c4124302652
yang <zy892065502@gmail.com>
2024-03-07 09:52:08 +0800

    change getprocess

--- content-script/Upload.tsx
+++ content-script/Upload.tsx
@@ -15,11 +15,15 @@ const Upload = ({ onScreenChange }) => {
     // 请求当前进度的函数
     const requestProgress = async () => {
       try {
-        const response = await browser.runtime.sendMessage({ action: 'getProcessed' });
-        if (response && response.process !== undefined) {
-          setProgress(response.process); // 更新进度
-          setLoading(response.process < 100); // 根据进度决定是否继续显示加载状态
-          if (response.process >= 100) {
+        chrome.storage.local.get(['process'], function(result){
+          if(result.process){
+            setProgress(result.process);
+          }
+        })
+        if (progress !== 0) {
+          setProgress(progress); // 更新进度
+          setLoading(progress < 100); // 根据进度决定是否继续显示加载状态
+          if (progress >= 100) {
             clearInterval(intervalId);
             onScreenChange();
           }
@@ -41,13 +45,15 @@ const Upload = ({ onScreenChange }) => {
   return (
     <>
       <div className='flex flex-col items-center gap-y-4 py-6'>
-        <button
-          className="bg-indigo-500 text-white text-lg font-bold py-2 px-4 rounded hover:bg-indigo-700 focus:outline-none focus:shadow-outline"
-          onClick={handleStartUpload}>
-          Reconstruct Your Old Bookmarks
-        </button>
+        {progress == 0 && (
+          <button
+            className="bg-indigo-500 text-white text-lg font-bold py-2 px-4 rounded hover:bg-indigo-700 focus:outline-none focus:shadow-outline"
+            onClick={handleStartUpload}>
+            Reconstruct Your Old Bookmarks
+          </button>
+        )}
 
-        {progress>1 && (
+        {progress > 1 && (
           <>
             <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
               <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${progress}%` }}> Doing... {progress}% </div>
@@ -55,7 +61,7 @@ const Upload = ({ onScreenChange }) => {
             <p className="text-slate-700">Please wait, in progress...</p>
           </>
         )}
-        {progress>=100 && (
+        {progress >= 100 && (
           <p className="text-slate-700">Completed!</p>
         )}
       </div>
